008639b0a83e2cedf34571ed1bf208dc1ea95505
carolina <carolina@carolina-POSITIVO-MOBILE>
2013-09-10 15:18:25 -0300

    identação-remove_projeto_base

--- cel/aplicacao/remove_projeto_base.php
+++ cel/aplicacao/remove_projeto_base.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 
 include("funcoes_genericas.php");
@@ -7,39 +6,38 @@ include_once("CELConfig/CELConfig.inc");
 
 
 
-//Cenário  -  Remover Projeto da base
-
-//Objetivo:	   Efetuar a remoção de um projeto da base de dados
+//Cenï¿½rio  -  Remover Projeto da base
+//Objetivo:	   Efetuar a remoï¿½ï¿½o de um projeto da base de dados
 //Contexto:	   Um Administrador de projeto deseja remover um determinado projeto da base de dados
-//                 Pré-Condição: Login, Ser administrador do projeto selecionado, ter selecionado o projeto para remoção em remove_projeto.php.  
+//                 Prï¿½-Condiï¿½ï¿½o: Login, Ser administrador do projeto selecionado, ter selecionado o projeto para remoï¿½ï¿½o em remove_projeto.php.  
 //Atores:	   Administrador
 //Recursos:	   Sistema, dados do projeto, base de dados
-//Episódios:       O sistema apaga todos os dados referentes ao determinado projeto da sua base de dados.
+//Episï¿½dios:       O sistema apaga todos os dados referentes ao determinado projeto da sua base de dados.
 
 
-        $id_projeto = $_SESSION['id_projeto_corrente'];
-        
-        removeProjeto($id_projeto);    
+$id_projeto = $_SESSION['id_projeto_corrente'];
+
+removeProjeto($id_projeto);
 ?>
 <html>
-<script language="javascript1.3">
-function logoff()
-{		
-   location.href = "http://<?php print( CELConfig_ReadVar("HTTPD_ip") . "/" . CELConfig_ReadVar("CEL_dir_relativo") ); ?>index.php";
-}
-</script>
+    <script language="javascript1.3">
+        function logoff()
+        {
+            location.href = "http://<?php print( CELConfig_ReadVar("HTTPD_ip") . "/" . CELConfig_ReadVar("CEL_dir_relativo")); ?>index.php";
+        }
+    </script>
     <head>
         <title>Remover Projeto</title>
     </head>  
 
- <body>
- <center><b>Projeto apagado com sucesso.</b></center>   
- <p>
-    <a href="javascript:logoff();">Clique aqui para Sair</a>
- </p>
-<p>
-  <i><a href="showSource.php?file=remove_projeto_base.php">Veja o código fonte!</a></i> 
-</p>
+    <body>
+    <center><b>Projeto apagado com sucesso.</b></center>   
+    <p>
+        <a href="javascript:logoff();">Clique aqui para Sair</a>
+    </p>
+    <p>
+        <i><a href="showSource.php?file=remove_projeto_base.php">Veja o cï¿½digo fonte!</a></i> 
+    </p>
 </body>
 </html>
 
