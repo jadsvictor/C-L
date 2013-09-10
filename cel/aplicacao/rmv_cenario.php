a507c960a44339a682514998e1fabe2c952fbfa4
carolina <carolina@carolina-POSITIVO-MOBILE>
2013-09-10 15:19:00 -0300

    identação-rmv_cenario

--- cel/aplicacao/rmv_cenario.php
+++ cel/aplicacao/rmv_cenario.php
@@ -1,5 +1,4 @@
 <?php
-
 // rmv_cenario.php: Este script faz um pedido de remover um cenario do projeto.
 // Arquivo chamador: main.php
 
@@ -10,38 +9,34 @@ include("httprequest.inc");
 chkUser("index.php");        // Checa se o usuario foi autenticado
 
 inserirPedidoRemoverCenario($_SESSION['id_projeto_corrente'], $id_cenario, $_SESSION['id_usuario_corrente']);
-
 ?>  
 
 <script language="javascript1.3">
 
-opener.parent.frames['code'].location.reload();
-opener.parent.frames['text'].location.replace('main.php?id_projeto=<?=$_SESSION['id_projeto_corrente']?>');
+    opener.parent.frames['code'].location.reload();
+    opener.parent.frames['text'].location.replace('main.php?id_projeto=<?= $_SESSION['id_projeto_corrente'] ?>');
 
 <?php
-
-// Cenário -  Excluir Cenário 
-
-//Objetivo:	Permitir ao Usuário Excluir um cenário que esteja ativo
-//Contexto:	Usuário deseja excluir um cenário
-//              Pré-Condição: Login, cenário cadastrado no sistema
-//Atores:	Usuário, Sistema
+// Cenï¿½rio -  Excluir Cenï¿½rio 
+//Objetivo:	Permitir ao Usuï¿½rio Excluir um cenï¿½rio que esteja ativo
+//Contexto:	Usuï¿½rio deseja excluir um cenï¿½rio
+//              Prï¿½-Condiï¿½ï¿½o: Login, cenï¿½rio cadastrado no sistema
+//Atores:	Usuï¿½rio, Sistema
 //Recursos:	Dados informados
-//Episódios:	O sistema fornecerá uma tela para o usuário justificar a necessidade daquela
-//              exclusão para que o administrador possa ler e aprovar ou não a mesma.
-//              Esta tela também conterá um botão para a confirmação da exclusão.
-//              Restrição: Depois de clicar no botão, o sistema verifica se todos os campos foram preenchidos 
-//Exceção:	Se todos os campos não foram preenchidos, retorna para o usuário uma mensagem
-//              avisando que todos os campos devem ser preenchidos e um botão de voltar para a pagina anterior.
-
+//Episï¿½dios:	O sistema fornecerï¿½ uma tela para o usuï¿½rio justificar a necessidade daquela
+//              exclusï¿½o para que o administrador possa ler e aprovar ou nï¿½o a mesma.
+//              Esta tela tambï¿½m conterï¿½ um botï¿½o para a confirmaï¿½ï¿½o da exclusï¿½o.
+//              Restriï¿½ï¿½o: Depois de clicar no botï¿½o, o sistema verifica se todos os campos foram preenchidos 
+//Exceï¿½ï¿½o:	Se todos os campos nï¿½o foram preenchidos, retorna para o usuï¿½rio uma mensagem
+//              avisando que todos os campos devem ser preenchidos e um botï¿½o de voltar para a pagina anterior.
 ?>
 
 </script>
 
-<h4>Operação efetuada com sucesso!</h4>
+<h4>Operaï¿½ï¿½o efetuada com sucesso!</h4>
 
 <script language="javascript1.3">
 
-self.close();
+    self.close();
 
 </script>
