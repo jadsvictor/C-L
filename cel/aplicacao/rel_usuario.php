c3a7176a27a0e1852b24066e72bacd8bd170b4ce
carolinaramalho <carol15022@hotmail.com>
2013-09-15 13:55:48 -0300

    Warnings, comentarios e mudanca de variaveis remove_projeto

--- cel/aplicacao/rel_usuario.php
+++ cel/aplicacao/rel_usuario.php
@@ -1,12 +1,10 @@
 <?php
 // Cenario - Relacionar usuarios ao projeto
-
 // Objetivo:  Permitir ao Administrador relacionar novos usuarios cadastrados ao projeto selecionado.
 // Contexto:  O Administrador deseja relacionar novos usuarios cadastrados ao projeto selecionado.
 //            Pre-Condicoes: Ser administrador do projeto que deseja relacionar os usuarios
 // Atores:    Administrador
 // Recursos:  Usuarios cadastrados
-
 
 session_start();
 
@@ -18,21 +16,21 @@ chkUser("index.php");
 $r = bd_connect() or die("Erro ao conectar ao SGBD");
 
 $submit = 0;
-if (isset($submit)) { 
+if (isset($submit)) {
     $delete_user = "DELETE FROM participa
           WHERE id_usuario != " . $_SESSION['id_usuario_corrente'] . "
           AND id_projeto = " . $_SESSION['id_projeto_corrente'];
     mysql_query($delete_user) or die("Erro ao executar a query de DELETE");
 
     $user = 0;
-    $number_selected_users = count($user); 
+    $number_selected_users = count($user);
     for ($i = 0; $i < $number_selected_users; $i++) {
         $user_registers = "INSERT INTO participa (id_usuario, id_projeto)
               VALUES (" . $user[$i] . ", " . $_SESSION['id_projeto_corrente'] . ")";
         mysql_query($user_registers) or die("Erro ao cadastrar usuario");
     }
     ?>
-     <script language="javascript1.3">
+    <script type ="text/javascript1.3">
 
         self.close();
 
@@ -47,12 +45,11 @@ if (isset($submit)) {
             <title>
                 Selecione os usuarios
             </title>
-            <script language="javascript1.3" src="MSelect.js">
+            <script type ="text/javascript1.3" src="MSelect.js">
             </script>
-            <script language="javascript1.3">
-
-                function createMSelect()
-                {
+            <script type ="text/javascript1.3">
+                    
+                function createMSelect() {
                     var usr_lselect = document.forms[0].elements['usuarios[]'];
                     var usr_rselect = document.forms[0].usuarios_r;
                     var usr_l2r = document.forms[0].usr_l2r;
@@ -60,8 +57,7 @@ if (isset($submit)) {
                     var MS_usr = new MSelect(usr_lselect, usr_rselect, usr_l2r, usr_r2l);
                 }
 
-                function selAll()
-                {
+                function selAll(){
                     var usuarios = document.forms[0].elements['usuarios[]'];
                     for (var i = 0; i < usuarios.length; i++)
                         usuarios.options[i].selected = true;
@@ -69,34 +65,34 @@ if (isset($submit)) {
 
             </script>
             <style>
-               select {
+                select {
                     width: 200px;
                     background-color: #CCFFFF
                 }
-             </style>
+            </style>
         </head>
         <body onLoad="createMSelect();">
             <h4>Selecione os usuarios para participar do projeto 
                 "<span style="color: orange">
-    <?= simple_query("nome", "projeto", "id_projeto = " . $_SESSION['id_projeto_corrente']) ?>
+                    <?= simple_query("nome", "projeto", "id_projeto = " . $_SESSION['id_projeto_corrente']) ?>
                 </span>":
             </h4>
             <p style="color: red">
                 Mantenha <strong>CTRL</strong> pressionado para selecionar multiplas opcoes
             </p>
             <form action="" method="post" onSubmit="selAll();">
-               table{
-                    border-spacing:10px;
-                    width="100%";
+                table{
+                border-spacing:10px;
+                width="100%";
                 }
                 td{
-                    align="center";
-                    style="color: green";
+                align="center";
+                style="color: green";
                 }
                 tr{
-                     align="center";
+                align="center";
                 }
-                
+
                 <table>
                     <tr>
                         <td>Participantes:</td>
@@ -106,23 +102,23 @@ if (isset($submit)) {
                     <tr>
                         <td rowspan="2">
                             <select name="usuarios[]" multiple size="6">
-    <?php
-    // Episodio 1: O Administrador clica no link Relacionar usuario ja existentes com este projeto.
-    $user_selects = "SELECT u.id_usuario, login
+                                <?php
+                                // Episodio 1: O Administrador clica no link Relacionar usuario ja existentes com este projeto.
+                                $user_selects = "SELECT u.id_usuario, login
                                               FROM usuario u, participa p
                                               WHERE u.id_usuario = p.id_usuario
                                               AND p.id_projeto = " . $_SESSION['id_projeto_corrente'] . "
                                               AND u.id_usuario != " . $_SESSION['id_usuario_corrente'];
 
-    $query_user_selects = mysql_query($user_selects) or die("Erro ao enviar a query");
-    while ($result_user_selects = mysql_fetch_array($query_user_selects)) {
-        ?>
+                                $query_user_selects = mysql_query($user_selects) or die("Erro ao enviar a query");
+                                while ($result_user_selects = mysql_fetch_array($query_user_selects)) {
+                                    ?>
                                     <option value="<?= $result_user_selects['id_usuario'] ?>">
-                                    <?= $result_user_selects['login'] ?>
+                                        <?= $result_user_selects['login'] ?>
                                     </option>
 
-                                      
-                               <?php
+
+                                    <?php
                                 }
 // Episodio 2: Excluindo usuario(s) do projeto: o administrador seleciona os usuarios cadastrados 
 //            (ja existentes) da lista Participantes (usuarios que pertencem a este projeto) 
@@ -136,33 +132,32 @@ if (isset($submit)) {
                         </td>
                         <td rowspan="2">
                             <select  multiple name="usuarios_r" size="6">
-    <?php
-    $user_selects_nonparticipating = "SELECT id_usuario FROM participa where participa.id_projeto =" . $_SESSION['id_projeto_corrente'];
-    $subqrr = mysql_query($user_selects_nonparticipating) or die("Erro ao enviar a subquery");
-    $result_user_nonparticipating = "(0)";
-    if ($subqrr != 0) {
-        $row = mysql_fetch_row($subqrr);
-        $result_user_nonparticipating = "( $row[0]";
-        while ($row = mysql_fetch_row($subqrr))
-            $result_user_nonparticipating = "$result_user_nonparticipating , $row[0]";
-        $result_user_nonparticipating = "$result_user_nonparticipating )";
-    }
-    $q = "SELECT usuario.id_usuario, usuario.login FROM usuario where usuario.id_usuario not in " . $result_user_nonparticipating;
-   
-    echo($q);
-    $query_user_nonparticipating = mysql_query($q) or die("Erro ao enviar a query");
-    while ($result = mysql_fetch_array($query_user_nonparticipating)) {
-        ?>
+                                <?php
+                                $user_selects_nonparticipating = "SELECT id_usuario FROM participa where participa.id_projeto =" . $_SESSION['id_projeto_corrente'];
+                                $subqrr = mysql_query($user_selects_nonparticipating) or die("Erro ao enviar a subquery");
+                                $result_user_nonparticipating = "(0)";
+                                if ($subqrr != 0) {
+                                    $row = mysql_fetch_row($subqrr);
+                                    $result_user_nonparticipating = "( $row[0]";
+                                    while ($row = mysql_fetch_row($subqrr))
+                                        $result_user_nonparticipating = "$result_user_nonparticipating , $row[0]";
+                                    $result_user_nonparticipating = "$result_user_nonparticipating )";
+                                }
+                                $q = "SELECT usuario.id_usuario, usuario.login FROM usuario where usuario.id_usuario not in " . $result_user_nonparticipating;
+
+                                echo($q);
+                                $query_user_nonparticipating = mysql_query($q) or die("Erro ao enviar a query");
+                                while ($result = mysql_fetch_array($query_user_nonparticipating)) {
+                                    ?>
                                     <option value="<?= $result['id_usuario'] ?>">
-                                    <?= $result['login'] ?>
+                                        <?= $result['login'] ?>
                                     </option>
 
-                               <?php
+                                    <?php
                                 }
 // Episodio 3: Incluindo usuario(s) ao projeto: o administrador seleciona os usuarios cadastrados 
 //           (ja existentes) da lista de usuarios que nao pertencem a este projeto e 
 //           clica no botao <- . 
-
                                 ?>
                             </select>
                         </td>
@@ -172,17 +167,17 @@ if (isset($submit)) {
                             <input name="usr_r2l" type="button" value="<-">
                         </td>
                     </tr>
-<?php
+    <?php
 // Episodio 4: Para atualizar os relacionamentos realizados, o administrador clica no botao Atualizar.
-?>
-                        _egg_logo_guid()
+    ?>
+                    _egg_logo_guid()
                     <tr>
                         <td align="center" colspan="3" height="50" valign="bottom">
                             <input name="submit" type="submit" value="Atualizar">
                         </td>
                     </tr>
                 </table>
-              </form>
+            </form>
             <br><i><a href="showSource.php?file=rel_usuario.php">Veja o codigo fonte!</a></i>
         </body>
     </html>
--- cel/aplicacao/remove_projeto.php
+++ cel/aplicacao/remove_projeto.php
@@ -1,4 +1,12 @@
 <?php
+//Cenário  -  Remover Projeto 
+
+//Objetivo:	   Permitir ao Administrador do projeto remover um projeto
+//Contexto:	   Um Administrador de projeto deseja remover um determinado projeto da base de dados
+//                 Pré-Condição: Login, Ser administrador do projeto selecionado.  
+//Atores:	   Administrador
+//Recursos:	   Sistema, dados do projeto, base de dados
+
 session_start();
 
 include("funcoes_genericas.php");
@@ -10,15 +18,18 @@ include("httprequest.inc");
         <title>Remover Projeto</title>
     </head>
 <?php
-$id_projeto = $_SESSION['id_projeto_corrente'];
+$id_project = $_SESSION['id_projeto_corrente'];
 
-$r = bd_connect() or die("Erro ao conectar ao SGBD");
-$seleciona_projeto = "SELECT * FROM projeto WHERE id_projeto = '$id_projeto' ";
-$query_seleciona_projeto = mysql_query($seleciona_projeto) or die("Erro ao enviar a query de select no projeto");
-$resultArrayProjeto = mysql_fetch_array($query_seleciona_projeto);
-$nome_Projeto = $resultArrayProjeto[1];
-$data_Projeto = $resultArrayProjeto[2];
-$descricao_Projeto = $resultArrayProjeto[3];
+bd_connect() or die("Erro ao conectar ao SGBD");
+$select_project = "SELECT * FROM projeto WHERE id_projeto = '$id_project' ";
+$query_select_project = mysql_query($select_project) or die("Erro ao enviar a query de select no projeto");
+$resultArrayProject = mysql_fetch_array($query_select_project);
+$name_Project = $resultArrayProject[1];
+$date_Project = $resultArrayProject[2];
+$project_description = $resultArrayProject[3];
+
+
+//Episodio 1:   O Administrador clica na opcao remover projeto encontrada no menu superior.
 ?>    
     <body>
         <h4>Remover Projeto:</h4>
@@ -31,6 +42,11 @@ $descricao_Projeto = $resultArrayProjeto[3];
        td{
         width="29%";
        }
+<?php
+// Episodio 2: O sistema disponibiliza uma tela para o administrador ter certeza 
+// de que esta removendo o projeto correto.
+?>
+       _egg_logo_guid()
         <table>
             <tr> 
                 <td><b>Nome do Projeto:</b></td>
@@ -38,16 +54,22 @@ $descricao_Projeto = $resultArrayProjeto[3];
                 <td><b>Descricao do projeto;o</b></td>
             </tr>
             <tr> 
-                <td><?php echo $nome_Projeto; ?></td>
-                <td><?php echo $data_Projeto; ?></td>
-                <td><?php echo $descricao_Projeto; ?></td>
+                <td><?php echo $name_Project; ?></td>
+                <td><?php echo $date_Project; ?></td>
+                <td><?php echo $project_description; ?></td>
             </tr>
         </table>
         <br><br>
-    <center><b>Cuidado!O projeto sera apagado para todos seus usuarios!</b></center>
+<?php
+//Episodio 3:  O Administrador clica no link de remoção.
+?>
+  <h1 style="text-align:center;">Cuidado!O projeto sera apagado para todos seus usuarios!</h1>
     <p><br>
     </p>
-    <center><a href="remove_projeto_base.php">Apagar o projeto</a></center> 
+<?php
+//Episodio 4: O sistema chama a página que removerá o projeto do banco de dados.
+?>
+    <h1 style="text-align:center;"><a href="remove_projeto_base.php">Apagar o projeto</a></h1> 
     <p>
     <i><a href="showSource.php?file=remove_projeto.php">Veja o codigo fonte!</a></i> 
     </p>
