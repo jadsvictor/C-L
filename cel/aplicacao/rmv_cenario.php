da8601beab64df1a985582ac2f87745aaea67c23
carolinaramalho <carol15022@hotmail.com>
2013-09-15 14:13:28 -0300

    Warnings, comentarios e mudanca de variaveis nas classes de remocao

--- cel/aplicacao/rmv_cenario.php
+++ cel/aplicacao/rmv_cenario.php
@@ -1,4 +1,17 @@
 <?php
+// Cenário -  Excluir Cenário 
+//Objetivo:	Permitir ao Usuário Excluir um cenário que esteja ativo
+//Contexto:	Usuário deseja excluir um cenário
+//              Pré-Condição: Login, cenário cadastrado no sistema
+//Atores:	Usuário, Sistema
+//Recursos:	Dados informados
+//Episódios:	O sistema fornecerá uma tela para o usuário justificar a necessidade daquela
+//              exclusão para que o administrador possa ler e aprovar ou não a mesma.
+//              Esta tela também conterá um botão para a confirmação da exclusão.
+//Restrição:    Depois de clicar no botão, o sistema verifica se todos os campos foram preenchidos 
+//Exceção:	Se todos os campos não foram preenchidos, retorna para o usuário uma mensagem
+//              avisando que todos os campos devem ser preenchidos e um botão de voltar 
+//              para a pagina anterior.
 
 session_start();
 
@@ -6,11 +19,11 @@ include("funcoes_genericas.php");
 include("httprequest.inc");
 chkUser("index.php");  
 
-$id_cenario = 0;
-inserirPedidoRemoverCenario($_SESSION['id_projeto_corrente'], $id_cenario, $_SESSION['id_usuario_corrente']);
+$id_scene = 0;
+inserirPedidoRemoverCenario($_SESSION['id_projeto_corrente'], $id_scene, $_SESSION['id_usuario_corrente']);
 ?>  
 
-<script language="javascript1.3">
+<script type="text/javascript1.3">
 
     opener.parent.frames['code'].location.reload();
     opener.parent.frames['text'].location.replace('main.php?id_projeto=<?= $_SESSION['id_projeto_corrente'] ?>');
@@ -19,7 +32,7 @@ inserirPedidoRemoverCenario($_SESSION['id_projeto_corrente'], $id_cenario, $_SES
 
 <h4>Operacaoo efetuada com sucesso!</h4>
 
-<script language="javascript1.3">
+<script type="text/javascript1.3">
 
     self.close();
 
--- cel/aplicacao/rmv_lexico.php
+++ cel/aplicacao/rmv_lexico.php
@@ -1,4 +1,17 @@
 <?php
+//Cenários -  Excluir Léxico 
+//Objetivo:	Permitir ao Usuário Excluir uma palavra do léxico que esteja ativa
+//Contexto:	Usuário deseja excluir uma palavra do léxico
+//              Pré-Condição: Login, palavra do léxico cadastrada no sistema 
+//Atores:	Usuário, Sistema
+//Recursos:	Dados informados
+//Episódios:	O sistema fornecerá uma tela para o usuário justificar a necessidade
+//              daquela exclusão para que o administrador possa ler e aprovar ou não.
+//              Esta tela também conterá um botão para a confirmação da exclusão.
+//Restrição:    Depois de clicado o botão o sistema verifica se todos os campos foram preenchidos 
+//Exceção:	Se todos os campos não foram preenchidos, retorna para o usuário 
+//              uma mensagem avisando que todos os campos devem ser preenchidos 
+//              e um botão de voltar para a pagina anterior.
 
 session_start();
 
@@ -6,12 +19,12 @@ include("funcoes_genericas.php");
 include("httprequest.inc");
 chkUser("index.php");  
 
-$id_projeto = 0;
-$id_lexico = 0;
-inserirPedidoRemoverLexico($id_projeto, $id_lexico, $_SESSION['id_usuario_corrente']);
+$id_project = 0;
+$id_lexicon = 0;
+inserirPedidoRemoverLexico($id_project, $id_lexicon, $_SESSION['id_usuario_corrente']);
 ?>  
 
-<script language="javascript1.3">
+<script type="text/javascript1.3">
 
     opener.parent.frames['code'].location.reload();
     opener.parent.frames['text'].location.replace('main.php?id_projeto=<?= $_SESSION['id_projeto_corrente'] ?>');
@@ -20,7 +33,7 @@ inserirPedidoRemoverLexico($id_projeto, $id_lexico, $_SESSION['id_usuario_corren
 
 <h4>Operacao efetuada com sucesso!</h4>
 
-<script language="javascript1.3">
+<script type="text/javascript1.3">
 
     self.close();
 
