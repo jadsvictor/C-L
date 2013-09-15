28cf4f7c85617184972d9318ae92e71d4fb37b5a
Camila <Camila@PC-Camila>
2013-09-15 11:48:16 -0300

    priorizar clareza ao inves de concisão

--- cel/aplicacao/form_xml.php
+++ cel/aplicacao/form_xml.php
@@ -16,21 +16,9 @@ chkUser("index.php");        // Checa se o usuario foi autenticado
     </head><form action="gerador_xml.php" method="post">
 
         <h2>Propriedades do Relat�rio a ser Gerado:</h2>
-<?php
-//Cen�rio - Gerar Relat�rios XML 
-//Objetivo:    Permitir ao administrador gerar relat�rios em formato XML de um projeto,
-//          identificados por data.     
-//Contexto:    Gerente deseja gerar um relat�rio para um dos projetos da qual � administrador.
-//          Pr�-Condi��o: Login, projeto cadastrado.
-//Atores:    Administrador     
-//Recursos:    Sistema, dados do relat�rio, dados cadastrados do projeto, banco de dados.     
-//Epis�dios:O administrador clica na op��o de Gerar Relat�rio XML.
-//          Restri��o: Somente o Administrador do projeto pode ter essa fun��o vis�vel.
-//          O sistema fornece para o administrador uma tela onde dever� fornecer os dados
-//          do relat�rio para sua posterior identifica��o, como data e vers�o. 
-
-$today = getdate();
-?>
+        <?php
+        $today = getdate();
+        ?>
 
         &nbsp;Data da Vers�o:
         <?= $today['mday']; ?>/<?= $today['mon']; ?>/<?= $today['year']; ?>
