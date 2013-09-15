7874ef5556e18689404131245559bfb90ab99774
Camila <Camila@PC-Camila>
2013-09-15 11:45:22 -0300

    priorizar clareza ao inves de concisão, identação e deleção de comentários

--- cel/aplicacao/frame_inferior.php
+++ cel/aplicacao/frame_inferior.php
@@ -8,7 +8,7 @@ chkUser("index.php");        // Cenario: controle de acesso
 // id do respectivo, mostra os dados necess�rios 
 // no frame. 
 
-function frame_inferior($bd, $tipo, $id) {
+function frame_inferior($tipo, $id) {
     $search = "'<[\/\!]*?[^<>]*?>'si";
     $replace = "";
 
@@ -30,13 +30,13 @@ function frame_inferior($bd, $tipo, $id) {
                 <th>Cen�rios</th> 
             </tr> 
 
-        <?php
-        while ($row = mysql_fetch_row($tb_cenario)) {
-            // Retira as tags HTML de dentro do titulo do cenario 
-            $row[1] = preg_replace($search, $replace, $row[1]);
-            $link = "<a href=javascript:reCarrega" .
-                    "('main.php?id=$row[0]&t=c');><span style=\"font-variant: small-caps\">$row[1]</span></a>";
-            ?> 
+            <?php
+            while ($row = mysql_fetch_row($tb_cenario)) {
+                // Retira as tags HTML de dentro do titulo do cenario 
+                $row[1] = preg_replace($search, $replace, $row[1]);
+                $link = "<a href=javascript:reCarrega" .
+                        "('main.php?id=$row[0]&t=c');><span style=\"font-variant: small-caps\">$row[1]</span></a>";
+                ?> 
 
                 <td><?= $link ?></td> 
 
@@ -71,22 +71,22 @@ function frame_inferior($bd, $tipo, $id) {
                     <th>L�xicos</th> 
                 </tr> 
 
-            <?php
-            while (1) {
-                ?> 
+                <?php
+                while (1) {
+                    ?> 
 
                     <tr> 
 
-            <?php
-            if ($rowc = mysql_fetch_row($tb_cenario)) {
-                $rowc[1] = preg_replace($search, $replace, $rowc[1]);
-                $link = "<a href=javascript:reCarrega" .
-                        "('main.php?id=$rowc[0]&t=c');><span style=\"font-variant: small-caps\">$rowc[1]</span></a>";
-            } // if 
-            else {
-                $link = "";
-            } // else 
-            ?> 
+                        <?php
+                        if ($rowc = mysql_fetch_row($tb_cenario)) {
+                            $rowc[1] = preg_replace($search, $replace, $rowc[1]);
+                            $link = "<a href=javascript:reCarrega" .
+                                    "('main.php?id=$rowc[0]&t=c');><span style=\"font-variant: small-caps\">$rowc[1]</span></a>";
+                        } // if 
+                        else {
+                            $link = "";
+                        } // else 
+                        ?> 
 
                         <td><?= $link ?></td> 
 
@@ -103,72 +103,72 @@ function frame_inferior($bd, $tipo, $id) {
                         <td><?= $link ?></td> 
                         </td>                </tr> 
 
-                        <?php
-                        if (!( $rowc ) && !( $rowl )) {
-                            break;
-                        } // if 
-                    } // while 
-                } //elseif 
-                else if ($tipo == "oc") /* CONCEITO */ {
-                    $q = "SELECT   r.id_relacao, r.nome, predicado 
+                    <?php
+                    if (!( $rowc ) && !( $rowl )) {
+                        break;
+                    } // if 
+                } // while 
+            } //elseif 
+            else if ($tipo == "oc") /* CONCEITO */ {
+                $q = "SELECT   r.id_relacao, r.nome, predicado 
                  FROM     conceito c, relacao_conceito rc, relacao r 
                  WHERE    c.id_conceito = $id 
                  AND      c.id_conceito = rc.id_conceito 
                  AND      r.id_relacao = rc.id_relacao 
                  ORDER BY r.nome  ";
-                    $result = mysql_query($q) or die("Erro ao enviar a query de selecao !!" . mysql_error());
+                $result = mysql_query($q) or die("Erro ao enviar a query de selecao !!" . mysql_error());
 
-                    print "<TABLE><tr><th align=left CLASS=\"Estilo\">Rela��o</th><th align=left CLASS=\"Estilo\">Conceito</Th></tr>";
+                print "<TABLE><tr><th align=left CLASS=\"Estilo\">Rela��o</th><th align=left CLASS=\"Estilo\">Conceito</Th></tr>";
 
-                    while ($line = mysql_fetch_array($result, MYSQL_BOTH)) {
-                        print "<tr><td CLASS=\"Estilo\"><a href=\"main.php?id=$line[0]&t=or\">$line[1]</a></td><td>$line[2]</TD></tr>";
-                    }
-                } //elseif 
-                else if ($tipo == "or") /* RELA��O */ {
-                    $q = "SELECT DISTINCT  c.id_conceito, c.nome 
+                while ($line = mysql_fetch_array($result, MYSQL_BOTH)) {
+                    print "<tr><td CLASS=\"Estilo\"><a href=\"main.php?id=$line[0]&t=or\">$line[1]</a></td><td>$line[2]</TD></tr>";
+                }
+            } //elseif 
+            else if ($tipo == "or") /* RELA��O */ {
+                $q = "SELECT DISTINCT  c.id_conceito, c.nome 
                  FROM     conceito c, relacao_conceito rc, relacao r 
                  WHERE    r.id_relacao = $id 
                  AND      c.id_conceito = rc.id_conceito 
                  AND      r.id_relacao = rc.id_relacao 
                  ORDER BY r.nome  ";
-                    $result = mysql_query($q) or die("Erro ao enviar a query de selecao !!" . mysql_error());
+                $result = mysql_query($q) or die("Erro ao enviar a query de selecao !!" . mysql_error());
 
-                    print "<TABLE><tr><th align=left CLASS=\"Estilo\">Conceitos</th></tr>";
+                print "<TABLE><tr><th align=left CLASS=\"Estilo\">Conceitos</th></tr>";
 
-                    while ($line = mysql_fetch_array($result, MYSQL_BOTH)) {
-                        print "<tr><td CLASS=\"Estilo\"><a href=\"main.php?id=$line[0]&t=oc\">$line[1]</a></td></tr>";
-                    }
-                } //elseif 
-                else if ($tipo == "oa") /* AXIOMA */ {
+                while ($line = mysql_fetch_array($result, MYSQL_BOTH)) {
+                    print "<tr><td CLASS=\"Estilo\"><a href=\"main.php?id=$line[0]&t=oc\">$line[1]</a></td></tr>";
+                }
+            } //elseif 
+            else if ($tipo == "oa") /* AXIOMA */ {
 
-                    $q = "SELECT   * 
+                $q = "SELECT   * 
                  FROM     axioma
                  WHERE    id_axioma = \"$id\";";
 
-                    $result = mysql_query($q) or die("Erro ao enviar a query de selecao !!" . mysql_error());
+                $result = mysql_query($q) or die("Erro ao enviar a query de selecao !!" . mysql_error());
 
-                    print "<TABLE><tr><th align=left CLASS=\"Estilo\">Conceito</th><th align=left CLASS=\"Estilo\">Conceito disjunto</th></tr>";
+                print "<TABLE><tr><th align=left CLASS=\"Estilo\">Conceito</th><th align=left CLASS=\"Estilo\">Conceito disjunto</th></tr>";
 
-                    while ($line = mysql_fetch_array($result, MYSQL_BOTH)) {
-                        $axi = explode(" disjoint ", $line[1]);
+                while ($line = mysql_fetch_array($result, MYSQL_BOTH)) {
+                    $axi = explode(" disjoint ", $line[1]);
 
-                        $q1 = "SELECT * FROM conceito WHERE nome = \"$axi[0]\";";
-                        $result1 = mysql_query($q1) or die("Erro ao enviar a query de selecao !!" . mysql_error());
-                        $line1 = mysql_fetch_array($result1, MYSQL_BOTH);
-                        print "<tr><td CLASS=\"Estilo\"><a href=\"main.php?id=$line1[0]&t=oc\">$axi[0]</a></td>";
+                    $q1 = "SELECT * FROM conceito WHERE nome = \"$axi[0]\";";
+                    $result1 = mysql_query($q1) or die("Erro ao enviar a query de selecao !!" . mysql_error());
+                    $line1 = mysql_fetch_array($result1, MYSQL_BOTH);
+                    print "<tr><td CLASS=\"Estilo\"><a href=\"main.php?id=$line1[0]&t=oc\">$axi[0]</a></td>";
 
-                        $q2 = "SELECT * FROM conceito WHERE nome = \"$axi[1]\";";
-                        $result2 = mysql_query($q2) or die("Erro ao enviar a query de selecao !!" . mysql_error());
-                        $line2 = mysql_fetch_array($result2, MYSQL_BOTH);
-                        print "<td CLASS=\"Estilo\"><a href=\"main.php?id=$line2[0]&t=oc\">$axi[1]</a></td></tr>";
-                    }
-                } //elseif 
-                ?> 
+                    $q2 = "SELECT * FROM conceito WHERE nome = \"$axi[1]\";";
+                    $result2 = mysql_query($q2) or die("Erro ao enviar a query de selecao !!" . mysql_error());
+                    $line2 = mysql_fetch_array($result2, MYSQL_BOTH);
+                    print "<td CLASS=\"Estilo\"><a href=\"main.php?id=$line2[0]&t=oc\">$axi[1]</a></td></tr>";
+                }
+            } //elseif 
+            ?> 
 
         </table> 
 
-            <?php
-        }
+        <?php
+    }
 
 // procura_ref 
-        ?>
+    ?>
