1e72ec181a7b2fb63e7017435b143036d515852f
Camila <Camila@PC-Camila>
2013-09-15 11:24:53 -0300

    alguns warnings arrumados, mudança de nome de algumas variáveis

--- cel/aplicacao/enviar_senha.php
+++ cel/aplicacao/enviar_senha.php
@@ -13,11 +13,11 @@ include("httprequest.inc");
 //           Se o login informado for cadastrado, sistema consulta no banco de dados qual 
 //           o email e senha do login informado.           
 
-$r = bd_connect() or die("Erro ao conectar ao SGBD");
+$ConectaBanco = bd_connect() or die("Erro ao conectar ao SGBD");
+$login = 0;
+$BuscaLoginBanco = "SELECT * FROM usuario WHERE login='$login'";
 
-$q = "SELECT * FROM usuario WHERE login='$login'";
-
-$qrr = mysql_query($q) or die("Erro ao executar a query");
+$qrr = mysql_query($BuscaLoginBanco) or die("Erro ao executar a query");
 ?>
 
 <html>
@@ -27,62 +27,47 @@ $qrr = mysql_query($q) or die("Erro ao executar a query");
     </head>
 
     <body bgcolor="#FFFFFF">
-<?php
-if (!mysql_num_rows($qrr)) {
-    ?>
+        <?php
+        if (!mysql_num_rows($qrr)) {
+            ?>
             <p style="color: red; font-weight: bold; text-align: center">Login inexistente!</p>
         <center><a href="JavaScript:window.history.go(-1)">Voltar</a></center>
-            <?php
-        } else {
-            $row = mysql_fetch_row($qrr);
-            $nome = $row[1];
-            $mail = $row[2];
-            $login = $row[3];
-            $senha = $row[4];
+        <?php
+    } else {
+        $row = mysql_fetch_row($qrr);
+        $nome = $row[1];
+        $mail = $row[2];
+        $login = $row[3];
+        $senha = $row[4];
 
-// Cen�rio - Lembrar senha 
-//Objetivo:	 Permitir o usu�rio cadastrado, que esqueceu sua senha,  receber  a mesma por email	
-//Contexto:	 Sistema est� aberto, Usu�rio esqueceu sua senha Usu�rio na tela de lembran�a de 
-//           senha. 
-//           Pr�-Condi��o: Usu�rio ter acessado ao sistema	
-//Atores:	 Usu�rio, Sistema	
-//Recursos:	 Banco de Dados	
-//Epis�dios: Sistema envia a senha para o email cadastrado correspondente ao login que 
-//           foi informado pelo usu�rio.     
-//           Caso n�o exista nenhum login cadastrado igual ao informado pelo usu�rio, 
-//           sistema exibe mensagem de erro na tela dizendo que login � inexistente, e 
-//           exibe um bot�o voltar, que redireciona o usu�rio para a tela de login novamente.
-            //$Vemail = ini_set("SMTP","mail.gmail.com");  
-            //require("class.phpmailer.php");
-            // Seta o SMTP sem alterar o config
-            //ini_set("SMTP","mail.hotpop.com");
-            //Funcao que gera uma senha randomica de 6 caracteres
+        //Funcao que gera uma senha randomica de 6 caracteres
 
-            function gerarandonstring($n) {
-                $str = "ABCDEFGHIJKLMNOPQRSTUVXYWZabcdefghijklmnopqrstuvxywz0123456789";
-                $cod = "";
-                for ($a = 0; $a < $n; $a++) {
-                    $rand = rand(0, 61);
-                    $cod .= substr($str, $rand, 1);
-                }
-                return $cod;
+        function gerarandonstring($n) {
+            $str = "ABCDEFGHIJKLMNOPQRSTUVXYWZabcdefghijklmnopqrstuvxywz0123456789";
+            $cod = "";
+            for ($a = 0; $a < $n; $a++) {
+                $rand = rand(0, 61);
+                $cod .= substr($str, $rand, 1);
             }
+            return $cod;
+        }
 
 // Chamando a fun��o: gerarandonstring([quantidadedecaracteres])echo gerarandonstring(20);
-            // Gera uma nova senha rand�mica	
-            $nova_senha = gerarandonstring(6);
-            //Criptografa senha
-            $nova_senha_cript = md5($nova_senha);
+        // Gera uma nova senha rand�mica	
+        $nova_senha = gerarandonstring(6);
+        //Criptografa senha
+        $nova_senha_cript = md5($nova_senha);
 
-            // Substitui senha antiga pela nova senha no banco de dados
+        // Substitui senha antiga pela nova senha no banco de dados
 
-            $qUp = "update usuario set senha = '$nova_senha_cript' where login = '$login'";
-            $qrrUp = mysql_query($qUp) or die("Erro ao executar a query de update na tabela usuario");
+        $AtualizaSenhaBanco = "update usuario set senha = '$nova_senha_cript' where login = '$login'";
+        $ErroAtualizarSenhaBanco = mysql_query($AtualizaSenhaBanco) or
+                die("Erro ao executar a query de update na tabela usuario");
 
-            $corpo_email = "Caro $nome,\n Como solicitado, estamos enviando sua nova senha para acesso ao sistema C&L.\n\n login: $login \n senha: $nova_senha \n\n Para evitar futuros transtornos altere sua senha o mais breve poss�vel. \n Obrigado! \n Equipe de Suporte do C&L.";
-            $headers = "";
-            if (mail("$mail", "Nova senha do C&L", "$corpo_email", $headers)) {
-                ?>
+        $corpo_email = "Caro $nome,\n Como solicitado, estamos enviando sua nova senha para acesso ao sistema C&L.\n\n login: $login \n senha: $nova_senha \n\n Para evitar futuros transtornos altere sua senha o mais breve poss�vel. \n Obrigado! \n Equipe de Suporte do C&L.";
+        $headers = "";
+        if (mail("$mail", "Nova senha do C&L", "$corpo_email", $headers)) {
+            ?>
             <p style="color: red; font-weight: bold; text-align: center">Uma nova senha foi criada e enviada para seu e-mail cadastrado.</p>
             <center><a href="JavaScript:window.history.go(-2)">Voltar</a></center>
             <?php
