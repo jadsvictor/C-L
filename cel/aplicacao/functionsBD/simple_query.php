52e119e8a919790992e90151ea16b15343101636
carol <carol@Carol.Home>
2013-11-17 01:12:20 -0200

    Assert função simple_query

--- cel/aplicacao/functionsBD/simple_query.php
+++ cel/aplicacao/functionsBD/simple_query.php
@@ -7,6 +7,17 @@ include_once("seguranca.php");
 if (!(function_exists("simple_query"))) {
 
     function simple_query($field, $table, $where) {
+        //test if the variable is not null
+        assert($field != NULL);
+        assert($table != NULL);
+        assert($where != NULL);
+                
+        //test if a variable has the correct type
+        assert(is_string($field));
+        assert(is_string($table));
+        assert(is_string($where));
+        
+        
         $r = bd_connect() or die("Erro ao conectar ao SGBD");
         $q = "SELECT $field FROM $table WHERE $where";
         $qrr = mysql_query($q) or die("Erro ao enviar a query");
