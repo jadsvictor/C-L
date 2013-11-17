1ce47f47cfc00ffa7d0860971f8f957ee425f594
carol <carol@Carol.Home>
2013-11-17 01:05:12 -0200

    Assert função check_User_Authentication

--- cel/aplicacao/functionsBD/check_User_Authentication.php
+++ cel/aplicacao/functionsBD/check_User_Authentication.php
@@ -3,6 +3,12 @@
  if (!(function_exists("checkUserAuthentication"))) {
 
     function checkUserAuthentication($url) {
+        
+        //test if a variable has the correct type
+        assert(is_string($url));
+        
+        //test if the variable is not null
+        assert($url != NULL);
 
         if(!(isset($_SESSION["id_usuario_corrente"])))  {
            
