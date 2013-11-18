6e53874a6832061efc94d1d6ca2a7dad708e9312
carol <carol@Carol.Home>
2013-11-18 19:11:34 -0200

    Assert função inclui_lexico

--- cel/aplicacao/functionsLexic/inclui_lexico.php
+++ cel/aplicacao/functionsLexic/inclui_lexico.php
@@ -15,18 +15,42 @@ include_once("seguranca.php");
 if (!(function_exists("inclui_lexico"))) {
 
     function inclui_lexico($id_projeto, $nome, $nocao, $impacto, $sinonimos, $classificacao) {
+        //test if the variable is not null
+        assert($id_projeto != NULL);
+        assert($nome != NULL);
+        assert($nocao != NULL);
+        assert($impacto != NULL);
+        assert($sinonimos != NULL);
+        assert($classificacao != NULL);
+        //test if a variable has the correct type
+        assert(is_string($id_projeto));
+        assert(is_string($nome));
+        assert(is_string($nocao));
+        assert(is_string($impacto));
+        assert(is_string($sinonimos));
+        assert(is_string($classificacao));
+        
         $r = bd_connect() or die("Erro ao conectar ao SGBD<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+        //test if the variable is not null
+        assert($r != NULL);
+        
         $data = date("Y-m-d");
+        //test if the variable is not null
+        assert($data != NULL);
 
 
         $q = "INSERT INTO lexico (id_projeto, data, nome, nocao, impacto, tipo)
               VALUES ($id_projeto, '$data', '" . prepares_data(strtolower($nome)) . "',
 			  '" . prepares_data($nocao) . "', '" . prepares_data($impacto) . "', '$classificacao')";
-
+        //test if the variable is not null
+        assert($q != NULL);
+        
         mysql_query($q) or die("Erro ao enviar a query<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
 
         //sinonimo
         $newLexId = mysql_insert_id($r);
+        //test if the variable is not null
+        assert($newLexId != NULL);
 
 
         if (!is_array($sinonimos))
@@ -35,12 +59,23 @@ if (!(function_exists("inclui_lexico"))) {
         foreach ($sinonimos as $novoSin) {
             $q = "INSERT INTO sinonimo (id_lexico, nome, id_projeto)
                 VALUES ($newLexId, '" . prepares_data(strtolower($novoSin)) . "', $id_projeto)";
+            //test if the variable is not null
+            assert($q != NULL);
             mysql_query($q, $r) or die("Erro ao enviar a query<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
         }
 
         $q = "SELECT max(id_lexico) FROM lexico";
+        //test if the variable is not null
+        assert($q != NULL);
+        
         $qrr = mysql_query($q) or die("Erro ao enviar a query<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+        //test if the variable is not null
+        assert($qrr != NULL);
+        
         $result = mysql_fetch_row($qrr);
+        //test if the variable is not null
+        assert($result!= NULL);
+        
         return $result[0];
     }
 
