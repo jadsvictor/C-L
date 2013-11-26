5ce95207cc3db4d1403ab3902703fcfd9918fcfb
jads VICTOR <jadsvictor@hotmail.com>
2013-11-26 12:42:09 -0200

    ajustes na classe "seguranca.php"

--- cel/aplicacao/seguranca.php
+++ cel/aplicacao/seguranca.php
@@ -6,18 +6,16 @@ function escapes_metacharacters($string) {
 }
 
 function prepares_data($string) {
-    //Remove espacos em branco do inicio e do fim da string
-    //$string = trim( $string );
-    // Substitui o & por amp; (para que nao de problemas ao gerar o XML)
-
+  
+    // Remove blank spaces from the beginning and end of string
+    // $ string = trim ($ string);
+    // Replaces by & amp; (so not a problem generating the XML)
     $string = ereg_replace("&", "&amp;", $string);
 
-    // Retira tags html e php da string
-
+    // Remove html tags and php string
     $string = strip_tags($string);
 
-    // Verifica se a diretiva get_magic_quotes_gpc() esta ativada, se estiver a funcao stripslashes e utilizada na string
-
+    // Check if the policy get_magic_quotes_gpc () is enabled, the stripslashes function and if used in string
     $string = get_magic_quotes_gpc() ? stripslashes($string) : $string;
     $string = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($string) : mysql_escape_string($string);
     return $string;
